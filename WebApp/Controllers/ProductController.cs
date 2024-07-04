72a1f44aa8f010c42b883e0d01be070ce8ebcf4a
vagiz <ahatovvagiz@gmail.com>
2024-07-05 00:07:36 +0300

    Добавьте файлы проекта.

---
+++ WebApp/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> AddProduct(string name, string description, decimal price)
+        {
+            using (StorageContext storageContext = new StorageContext())
+            {
+                if (storageContext.Products.Any(p => p.Name == name))
+                    return StatusCode(409);
+
+                var product = new Product() { Name = name, Description = description, Price = price };
+                storageContext.Products.Add(product);
+                storageContext.SaveChanges();
+                return Ok(product.Id);
+            }
+        }
+        [HttpGet("get_all_products")]
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
+        {
+            using (StorageContext storageContext = new StorageContext())
+            {
+                var list = storageContext.Products.Select(p => new Product { Name = p.Name, Description = p.Description, Price = p.Price }).ToList();
+                return Ok(list);
+            }
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            using (StorageContext storageContext = new StorageContext())
+            {
+                var product = storageContext.Products.FirstOrDefault(p => p.Id == id);
+                if (product == null)
+                    return NotFound();
+
+                storageContext.Products.Remove(product);
+                storageContext.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpPatch("{id}")]
+        public ActionResult ChangePrice(int id, decimal newPrice)
+        {
+            using (StorageContext storageContext = new StorageContext())
+            {
+                var product = storageContext.Products.FirstOrDefault(p => p.Id == id);
+                if (product == null)
+                    return NotFound();
+
+                product.Price = newPrice;
+                storageContext.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+}
---
+++ WebApp/Data/StorageContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class StorageContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductGroup> ProductGroups { get; set; }
+        public virtual DbSet<Storage> Storages { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Data Source =.\\SQLEXPRESS; Initial Catalog = Products; Trusted_Connection=True; TrustServerCertificate=True").UseLazyLoadingProxies().LogTo(Console.WriteLine);
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Host=localhost;Database=web_lesson1;Username=postgres;Password=example").UseLazyLoadingProxies().LogTo(Console.WriteLine);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductGroup>(entity =>
+            {
+
+                entity.HasKey(pg => pg.Id)
+                      .HasName("product_group_pk");
+
+                entity.ToTable("category");
+
+                entity.Property(pg => pg.Name)
+                      .HasColumnName("name")
+                      .HasMaxLength(255);
+            });
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.Id)
+                      .HasName("product_pk");
+
+                entity.Property(p => p.Name)
+                      .HasColumnName("name")
+                      .HasMaxLength(255);
+
+                entity.HasOne(p => p.ProductGroup).WithMany(p => p.Products).HasForeignKey(p => p.ProductGroupId);
+            });
+            modelBuilder.Entity<Storage>(entity =>
+            {
+                entity.HasKey(s => s.Id)
+                      .HasName("storage_pk");
+
+                entity.HasOne(s => s.Product).WithMany(p => p.Storages).HasForeignKey(p => p.ProductId);
+            });
+
+        }
+    }
+}
